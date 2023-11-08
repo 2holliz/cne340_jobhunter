e6d8dc0686f0e7d2d9d7ae58ee90d3de1682dea4
Nmclendon <nicholas.a.mclendon@gmail.com>
2023-11-07 16:58:49 -0800

    Update jobhunter.py

--- jobhunter.py
+++ jobhunter.py
@@ -19,8 +19,7 @@ def create_tables(cursor):
     # Must set Title to CHARSET utf8 unicode Source: http://mysql.rjweb.org/doc.php/charcoll.
     # Python is in latin-1 and error (Incorrect string value: '\xE2\x80\xAFAbi...') will occur if Description is not in unicode format due to the json data
     cursor.execute('''CREATE TABLE IF NOT EXISTS jobs (id INT PRIMARY KEY auto_increment, Job_id varchar(50) , 
-    company varchar (300), Created_at DATE, url varchar(30000), Title LONGBLOB CHARACTER SET utf8, Description LONGBLOB CHARACTER SET utf8); ''')
-
+        company varchar (300), Created_at DATE, url varchar(30000), Title LONGBLOB, Description LONGBLOB ); ''')
 
 # Query the database.
 # You should not need to edit anything in this function
@@ -52,7 +51,7 @@ def notify_user(jobdetails):
 def check_if_job_exists(cursor, jobdetails):
     ##Add your code here
     query = 'SELECT * FROM jobs WHERE Job_id = %s'
-    cursor.execute(query, (jobdetails['job_id'],))
+    cursor.execute(query, (jobdetails['Job_id'],))
     exists = cursor.fetchone()
     if exists:
         return True
@@ -72,7 +71,8 @@ def delete_job(cursor, jobdetails):
 # Grab new jobs from a website, Parses JSON code and inserts the data into a list of dictionaries do not need to edit
 def fetch_new_jobs():
     query = requests.get("https://remotive.io/api/remote-jobs")
-    datas = json.loads(query.text)
+    datas = json.loads(query.text)#
+    print(datas)
     return datas
 
 
@@ -81,13 +81,14 @@ def jobhunt(cursor):
     # Fetch jobs from website
     jobpage = fetch_new_jobs()  # Gets API website and holds the json data in it as a list
     # use below print statement to view list in json format
-    print(jobpage)
+#    print(jobpage)
     add_or_delete_job(jobpage, cursor)
 
 
 def add_or_delete_job(jobpage, cursor):
     # Add your code here to parse the job page
     for jobdetails in jobpage['jobs']:  # EXTRACTS EACH JOB FROM THE JOB LIST. It errored out until I specified jobs. This is because it needs to look at the jobs dictionary from the API. https://careerkarma.com/blog/python-typeerror-int-object-is-not-iterable/
+        print(jobdetails)
         # Add in your code here to check if the job already exists in the DB
         exists = check_if_job_exists(cursor, jobdetails)
 #        is_job_found = len(
@@ -113,6 +114,7 @@ def main():
 
     while (1):  # Infinite Loops. Only way to kill it is to crash or manually crash it. We did this as a background process/passive scraper
         jobhunt(cursor)
+        cursor.commit()
         time.sleep(21600)  # Sleep for 1h, this is ran every hour because API or web interfaces have request limits. Your reqest will get blocked.
 
 
